97ed0a072d93b1559f27a572d551e60f6d4c9e07
Rahul <rchadalawada@sg.palo-it.com>
2021-02-26 14:51:25 +0800

    Initial Commit

---
+++ Demo IOS App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo IOS App
+//
+//  Created by Rahul Chadalawada on 26/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo IOS App/Demo_IOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_IOS_AppApp.swift
+//  Demo IOS App
+//
+//  Created by Rahul Chadalawada on 26/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Demo_IOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
